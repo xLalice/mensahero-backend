a487460a7850abe677478741c9d5b05eecf94fba
John Lorenz <dreamsend08@gmail.com>
2024-09-20 15:46:54 +0800

    fix: remove dist

--- dist/middleware/auth.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (req, res, next) => {
-    if (req.isAuthenticated()) {
-        return next();
-    }
-    console.log(req.user);
-    res.status(401).json({ message: 'Unauthorized' });
-};
